ce1f5a1a2b9c0d82afe07007a71941250a96cdfa
tophantus <tophantus@gmail.com>
2025-06-02 15:03:31 +0700

    first commit

---
+++ backend/SamuChat/samuchat/src/main/java/com/shogun/samuchat/SamuChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.shogun.samuchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuChatApiApplication.class, args);
+	}
+
+}
